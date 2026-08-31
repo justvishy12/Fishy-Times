ee046f79eb86fa030156e2ffccfc5582a101b03a
Fishyyyyyyyyy <justvishy.v@gmail.com>
2026-08-30 19:00:25 -0500

    Added Check Feature
    Now the whole game is playable with a check I just need to add if they get it wrong or letter in wrong place

---
+++ ocean-words/scripts/oceanwordsplay.gd
@@ -0,0 +1,414 @@
+extends Node2D
+
+var words = []
+var used_words = []
+var answer = ""
+var guess = ""
+var row1 = false
+var row2 = false
+var row3 = false
+var row4 = false
+var row5 = false
+var c1 = false
+var c2 = false
+var c3 = false
+var c4 = false
+var c5 = false
+var submit_answer = ""
+var game_done = false
+	
+func load_words():
+	var file = FileAccess.open("res://words.txt", FileAccess.READ)
+	
+	while not file.eof_reached():
+		var word = file.get_line().strip_edges()
+		
+		if word != "":
+			words.append(word)
+
+func save_data():
+	var file = FileAccess.open("user://wordle_save.json", FileAccess.WRITE)
+	file.store_string(JSON.stringify(used_words))
+
+func load_data():
+	if FileAccess.file_exists("user://wordle_save.json"):
+		var file = FileAccess.open("user://wordle_save.json", FileAccess.READ)
+		used_words = JSON.parse_string(file.get_as_text())
+
+func _ready():
+	load_words()
+	load_data()
+	pick_word()
+	print(answer)
+
+func pick_word():
+	var available_words = []
+	
+	for word in words:
+		if word not in used_words:
+			available_words.append(word)
+			
+	if available_words.is_empty():
+		used_words.clear()
+		save_data()
+		available_words = words.duplicate()
+	answer = available_words.pick_random()
+
+func game_over():
+	c1 = false
+	c2 = false
+	c3 = false
+	c4 = false
+	c5 = false
+	guess = ""
+	row1 = true
+	row2 = true
+	row3 = true
+	row4 = true
+	row5 = true
+	used_words.append(answer)
+	save_data()
+	
+#game actually starting
+func check():
+	submit_answer = guess
+	if row1 == false:
+		row1 = true
+		c1 = false
+		c2 = false
+		c3 = false
+		c4 = false
+		c5 = false
+		if guess[0] == answer[0]:
+			$"Colored Boxes/Row1/BlackWord1".modulate = Color(0.157, 0.604, 0.157)
+		if guess[1] == answer[1]:
+			$"Colored Boxes/Row1/BlackWord2".modulate = Color(0.157, 0.604, 0.157)
+		if guess[2] == answer[2]:
+			$"Colored Boxes/Row1/BlackWord3".modulate = Color(0.157, 0.604, 0.157)
+		if guess[3] == answer[3]:
+			$"Colored Boxes/Row1/BlackWord4".modulate = Color(0.157, 0.604, 0.157)
+		if guess[4] == answer[4]:
+			$"Colored Boxes/Row1/BlackWord5".modulate = Color(0.157, 0.604, 0.157)
+		guess = ""
+	elif row2 == false:
+		row2 = true
+		c1 = false
+		c2 = false
+		c3 = false
+		c4 = false
+		c5 = false
+		if guess[0] == answer[0]:
+			$"Colored Boxes/Row2/BlackWord1".modulate = Color(0.157, 0.604, 0.157)
+		if guess[1] == answer[1]:
+			$"Colored Boxes/Row2/BlackWord2".modulate = Color(0.157, 0.604, 0.157)
+		if guess[2] == answer[2]:
+			$"Colored Boxes/Row2/BlackWord3".modulate = Color(0.157, 0.604, 0.157)
+		if guess[3] == answer[3]:
+			$"Colored Boxes/Row2/BlackWord4".modulate = Color(0.157, 0.604, 0.157)
+		if guess[4] == answer[4]:
+			$"Colored Boxes/Row2/BlackWord5".modulate = Color(0.157, 0.604, 0.157)
+		guess = ""
+	elif row3 == false:
+		row3 = true
+		c1 = false
+		c2 = false
+		c3 = false
+		c4 = false
+		c5 = false
+		if guess[0] == answer[0]:
+			$"Colored Boxes/Row3/BlackWord1".modulate = Color(0.157, 0.604, 0.157)
+		if guess[1] == answer[1]:
+			$"Colored Boxes/Row3/BlackWord2".modulate = Color(0.157, 0.604, 0.157)
+		if guess[2] == answer[2]:
+			$"Colored Boxes/Row3/BlackWord3".modulate = Color(0.157, 0.604, 0.157)
+		if guess[3] == answer[3]:
+			$"Colored Boxes/Row3/BlackWord4".modulate = Color(0.157, 0.604, 0.157)
+		if guess[4] == answer[4]:
+			$"Colored Boxes/Row3/BlackWord5".modulate = Color(0.157, 0.604, 0.157)
+		guess = ""
+	elif row4 == false:
+		row4 = true
+		c1 = false
+		c2 = false
+		c3 = false
+		c4 = false
+		c5 = false
+		if guess[0] == answer[0]:
+			$"Colored Boxes/Row4/BlackWord1".modulate = Color(0.157, 0.604, 0.157)
+		if guess[1] == answer[1]:
+			$"Colored Boxes/Row4/BlackWord2".modulate = Color(0.157, 0.604, 0.157)
+		if guess[2] == answer[2]:
+			$"Colored Boxes/Row4/BlackWord3".modulate = Color(0.157, 0.604, 0.157)
+		if guess[3] == answer[3]:
+			$"Colored Boxes/Row4/BlackWord4".modulate = Color(0.157, 0.604, 0.157)
+		if guess[4] == answer[4]:
+			$"Colored Boxes/Row4/BlackWord5".modulate = Color(0.157, 0.604, 0.157)
+		guess = ""
+	elif row5 == false:
+		row5 = true
+		c1 = false
+		c2 = false
+		c3 = false
+		c4 = false
+		c5 = false
+		if guess[0] == answer[0]:
+			$"Colored Boxes/Row5/BlackWord1".modulate = Color(0.157, 0.604, 0.157)
+		if guess[1] == answer[1]:
+			$"Colored Boxes/Row5/BlackWord2".modulate = Color(0.157, 0.604, 0.157)
+		if guess[2] == answer[2]:
+			$"Colored Boxes/Row5/BlackWord3".modulate = Color(0.157, 0.604, 0.157)
+		if guess[3] == answer[3]:
+			$"Colored Boxes/Row5/BlackWord4".modulate = Color(0.157, 0.604, 0.157)
+		if guess[4] == answer[4]:
+			$"Colored Boxes/Row5/BlackWord5".modulate = Color(0.157, 0.604, 0.157)
+		guess = ""
+	if submit_answer == answer:
+		game_over()
+func add_letter():
+	if row1 == false:
+		if c1 == false:
+			c1 = true
+			$"Row Letters/Row1/L1".text = guess[0].to_upper()
+		elif c2 == false:
+			c2 = true
+			$"Row Letters/Row1/L2".text = guess[1].to_upper()
+		elif c3 == false:
+			c3 = true
+			$"Row Letters/Row1/L5".text = guess[2].to_upper()
+		elif c4 == false:
+			c4 = true
+			$"Row Letters/Row1/L3".text = guess[3].to_upper()
+		elif c5 == false:
+			c5 = true
+			$"Row Letters/Row1/L4".text = guess[4].to_upper()
+	elif row2 == false:
+		if c1 == false:
+			c1 = true
+			$"Row Letters/Row2/L1".text = guess[0].to_upper()
+		elif c2 == false:
+			c2 = true
+			$"Row Letters/Row2/L2".text = guess[1].to_upper()
+		elif c3 == false:
+			c3 = true
+			$"Row Letters/Row2/L5".text = guess[2].to_upper()
+		elif c4 == false:
+			c4 = true
+			$"Row Letters/Row2/L3".text = guess[3].to_upper()
+		elif c5 == false:
+			c5 = true
+			$"Row Letters/Row2/L4".text = guess[4].to_upper()
+	elif row3 == false:
+		if c1 == false:
+			c1 = true
+			$"Row Letters/Row3/L1".text = guess[0].to_upper()
+		elif c2 == false:
+			c2 = true
+			$"Row Letters/Row3/L2".text = guess[1].to_upper()
+		elif c3 == false:
+			c3 = true
+			$"Row Letters/Row3/L5".text = guess[2].to_upper()
+		elif c4 == false:
+			c4 = true
+			$"Row Letters/Row3/L3".text = guess[3].to_upper()
+		elif c5 == false:
+			c5 = true
+			$"Row Letters/Row3/L4".text = guess[4].to_upper()
+	elif row4 == false:
+		if c1 == false:
+			c1 = true
+			$"Row Letters/Row4/L1".text = guess[0].to_upper()
+		elif c2 == false:
+			c2 = true
+			$"Row Letters/Row4/L2".text = guess[1].to_upper()
+		elif c3 == false:
+			c3 = true
+			$"Row Letters/Row4/L5".text = guess[2].to_upper()
+		elif c4 == false:
+			c4 = true
+			$"Row Letters/Row4/L3".text = guess[3].to_upper()
+		elif c5 == false:
+			c5 = true
+			$"Row Letters/Row4/L4".text = guess[4].to_upper()
+	elif row5 == false:
+		if c1 == false:
+			c1 = true
+			$"Row Letters/Row5/L1".text = guess[0].to_upper()
+		elif c2 == false:
+			c2 = true
+			$"Row Letters/Row5/L2".text = guess[1].to_upper()
+		elif c3 == false:
+			c3 = true
+			$"Row Letters/Row5/L5".text = guess[2].to_upper()
+		elif c4 == false:
+			c4 = true
+			$"Row Letters/Row5/L3".text = guess[3].to_upper()
+		elif c5 == false:
+			c5 = true
+			$"Row Letters/Row5/L4".text = guess[4].to_upper()
+			
+func _input(event):
+	if event.is_action_pressed("enter") and guess.length() == 5:
+		check()
+
+	if event.is_action_pressed("a") and guess.length() < 5:
+		guess += "a"
+		add_letter()
+	if event.is_action_pressed("b") and guess.length() < 5:
+		guess += "b"
+		add_letter()
+	if event.is_action_pressed("c") and guess.length() < 5:
+		guess += "c"
+		add_letter()
+	if event.is_action_pressed("d") and guess.length() < 5:
+		guess += "d"
+		add_letter()
+	if event.is_action_pressed("e") and guess.length() < 5:
+		guess += "e"
+		add_letter()
+	if event.is_action_pressed("f") and guess.length() < 5:
+		guess += "f"
+		add_letter()
+	if event.is_action_pressed("g") and guess.length() < 5:
+		guess += "g"
+		add_letter()
+	if event.is_action_pressed("h") and guess.length() < 5:
+		guess += "h"
+		add_letter()
+	if event.is_action_pressed("i") and guess.length() < 5:
+		guess += "i"
+		add_letter()
+	if event.is_action_pressed("j") and guess.length() < 5:
+		guess += "j"
+		add_letter()
+	if event.is_action_pressed("k") and guess.length() < 5:
+		guess += "k"
+		add_letter()
+	if event.is_action_pressed("l") and guess.length() < 5:
+		guess += "l"
+		add_letter()
+	if event.is_action_pressed("m") and guess.length() < 5:
+		guess += "m"
+		add_letter()
+	if event.is_action_pressed("n") and guess.length() < 5:
+		guess += "n"
+		add_letter()
+	if event.is_action_pressed("o") and guess.length() < 5:
+		guess += "o"
+		add_letter()
+	if event.is_action_pressed("p") and guess.length() < 5:
+		guess += "p"
+		add_letter()
+	if event.is_action_pressed("q") and guess.length() < 5:
+		guess += "q"
+		add_letter()
+	if event.is_action_pressed("r") and guess.length() < 5:
+		guess += "r"
+		add_letter()
+	if event.is_action_pressed("s") and guess.length() < 5:
+		guess += "s"
+		add_letter()
+	if event.is_action_pressed("t") and guess.length() < 5:
+		guess += "t"
+		add_letter()
+	if event.is_action_pressed("u") and guess.length() < 5:
+		guess += "u"
+		add_letter()
+	if event.is_action_pressed("v") and guess.length() < 5:
+		guess += "v"
+		add_letter()
+	if event.is_action_pressed("w") and guess.length() < 5:
+		guess += "w"
+		add_letter()
+	if event.is_action_pressed("x") and guess.length() < 5:
+		guess += "x"
+		add_letter()
+	if event.is_action_pressed("y") and guess.length() < 5:
+		guess += "y"
+		add_letter()
+	if event.is_action_pressed("z") and guess.length() < 5:
+		guess += "z"
+		add_letter()
+	if event.is_action_pressed("delete"):
+		if guess.length() > 0:
+			guess = guess.left(guess.length() - 1)
+			if row1 == false:
+				if c5 == true:
+					c5 = false
+					$"Row Letters/Row1/L4".text = ""
+				elif c4 == true:
+					c4 = false
+					$"Row Letters/Row1/L3".text = ""
+				elif c3 == true:
+					c3 = false
+					$"Row Letters/Row1/L5".text = ""
+				elif c2 == true:
+					c2 = false
+					$"Row Letters/Row1/L2".text = ""
+				elif c1 == true:
+					c1 = false
+					$"Row Letters/Row1/L1".text = ""
+			elif row2 == false:
+				if c5 == true:
+					c5 = false
+					$"Row Letters/Row2/L4".text = ""
+				elif c4 == true:
+					c4 = false
+					$"Row Letters/Row2/L3".text = ""
+				elif c3 == true:
+					c3 = false
+					$"Row Letters/Row2/L5".text = ""
+				elif c2 == true:
+					c2 = false
+					$"Row Letters/Row2/L2".text = ""
+				elif c1 == true:
+					c1 = false
+					$"Row Letters/Row2/L1".text = ""
+			elif row3 == false:
+				if c5 == true:
+					c5 = false
+					$"Row Letters/Row3/L4".text = ""
+				elif c4 == true:
+					c4 = false
+					$"Row Letters/Row3/L3".text = ""
+				elif c3 == true:
+					c3 = false
+					$"Row Letters/Row3/L5".text = ""
+				elif c2 == true:
+					c2 = false
+					$"Row Letters/Row3/L2".text = ""
+				elif c1 == true:
+					c1 = false
+					$"Row Letters/Row3/L1".text = ""
+			elif row4 == false:
+				if c5 == true:
+					c5 = false
+					$"Row Letters/Row4/L4".text = ""
+				elif c4 == true:
+					c4 = false
+					$"Row Letters/Row4/L3".text = ""
+				elif c3 == true:
+					c3 = false
+					$"Row Letters/Row4/L5".text = ""
+				elif c2 == true:
+					c2 = false
+					$"Row Letters/Row4/L2".text = ""
+				elif c1 == true:
+					c1 = false
+					$"Row Letters/Row4/L1".text = ""
+			elif row5 == false:
+				if c5 == true:
+					c5 = false
+					$"Row Letters/Row5/L4".text = ""
+				elif c4 == true:
+					c4 = false
+					$"Row Letters/Row5/L3".text = ""
+				elif c3 == true:
+					c3 = false
+					$"Row Letters/Row5/L5".text = ""
+				elif c2 == true:
+					c2 = false
+					$"Row Letters/Row5/L2".text = ""
+				elif c1 == true:
+					c1 = false
+					$"Row Letters/Row5/L1".text = ""
+				
